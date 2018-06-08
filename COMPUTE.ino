f56c2d84cf59dc9da3b96f62a51303f5a679bc3b
snsd0805 <levi900227@gmail.com>
2018-06-08 23:29:18 +0800

    完成加法(Pin 13)功能

--- COMPUTE.ino
+++ COMPUTE.ino
@@ -7,9 +7,11 @@ const int CountPin = 8;
 const int CancelPin = 7;
 const int DecidePin = 6;
 int sum=0;
+int symble_type=0;
 int past_temp=0;
 int temp=0;
 int IfAnyPin=0;//判斷當下按鍵狀態
+String show="";//運算式
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,20 +29,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  count();    Serial.println("Round");
-  //symble();
+  count();   // Serial.println("Round");
+  symble();
 }
-/*
+
+//void compute(
+
 void symble(){
   while(1){
     if( digitalRead(13)==HIGH){
       if(IfAnyPin==0){
-        temp+=1;
-        Serial.println(temp);
+        show+='+';
+        symble_type=1;
+        Serial.println("+");
+        Serial.print("目前已輸入的運算式為：");
+        Serial.println(show);                
+        Serial.println("0");
         IfAnyPin=1;
         delay(100);
+        break;
       }
-    }else if(digitalRead(12)==HIGH){
+    }/*else if(digitalRead(12)==HIGH){
       
     }else if(digitalRead(11)==HIGH){
       
@@ -48,11 +57,12 @@ void symble(){
 
     }else if(digitalRead(9)==HIGH){
       
-    }else{
+    }*/else{
       IfAnyPin=0;
       delay(100);
     }
-}*/
+  }
+}
 
 void count(){
   while(1){
@@ -70,13 +80,18 @@ void count(){
 
     if(digitalRead(DecidePin)==HIGH){
       if(IfAnyPin==0){
-        past_temp=temp;
-        temp=0;
-        Serial.println(past_temp);
-        Serial.println(temp);
-        IfAnyPin=1;
-        delay(100);
-        break;
+        if(temp!=0){
+          past_temp=temp;
+          temp=0;
+          show+=past_temp;
+          Serial.print("輸入的數字為：");
+          Serial.println(show);
+          Serial.print("繼續輸入運算符號：");
+          //Serial.println(temp);
+          IfAnyPin=1;
+          delay(100);
+          break;
+        }
       }
     }else{
       IfAnyPin=0;
